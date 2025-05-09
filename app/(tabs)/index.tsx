978f62cc1f9b6f112d6f0b6e108d2f22a2b5d66f
Michael Luna <michaelluna244@gmail.com>
2025-05-08 21:33:49 -0700

    Refactor Home screen to display an image viewer and remove text placeholder; add ImageViewer component for better image handling.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,27 +1,27 @@
-import { Text, View, StyleSheet } from "react-native";
-import { Link } from 'expo-router';
+import { View, StyleSheet } from 'react-native';
+import ImageViewer from '@/components/ImageViewer';
+
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
+
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#25292e",
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: '#25292e',
+    alignItems: 'center',
   },
-  text: {
-    color: '#fff',
-  },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff',
+  imageContainer: {
+    flex: 1,
   },
 });
