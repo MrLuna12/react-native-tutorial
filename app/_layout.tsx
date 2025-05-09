fdb02cbd0fc9af610ae63e6116c062b66adb238d
Michael Luna <michaelluna244@gmail.com>
2025-05-08 20:12:59 -0700

    Add Not Found screen and Remove the Left header from the index page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,12 @@
 import { Stack } from "expo-router";
+import React from "react";
 
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: 'Home' }} />
+      <Stack.Screen name="index" options={{ title: 'Home', headerLeft: () => <></> }} />
       <Stack.Screen name="about" options={{ title: 'About' }} />
+      <Stack.Screen name="+not-found" />
     </Stack>
   );
 }
