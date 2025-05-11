6cf6700dc7b4b8914564cffca4786036aac7cb3b
Michael Luna <michaelluna244@gmail.com>
2025-05-10 19:07:46 -0700

    Integrate MediaLibrary for image saving; implement image reference handling in Index screen.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,8 +2,10 @@ import { View, StyleSheet } from 'react-native';
 import ImageViewer from '@/components/ImageViewer';
 import Button from '@/components/Button';
 import * as ImagePicker from 'expo-image-picker';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { type ImageSource } from 'expo-image';
+import * as MediaLibrary from 'expo-media-library';
+import { captureRef } from 'react-native-view-shot';
 
 import IconButton from '@/components/IconButton';
 import CircleButton from '@/components/CircleButton';
@@ -15,10 +17,16 @@ const PlaceholderImage = require('@/assets/images/background-image.png');
 
 
 export default function Index() {
+    const [status, requestPermission] = MediaLibrary.usePermissions();
     const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
     const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
     const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
     const [pickedEmoji, setPickedEmoji] = useState<ImageSource | undefined>(undefined);
+    const imageRef = useRef<View>(null);
+
+    if (status === null) {
+        requestPermission();
+    }
 
     const pickImageAsync = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
@@ -47,16 +55,30 @@ export default function Index() {
         setIsModalVisible(false);
     };
 
-      const onSaveImageAsync = async () => {
-    // we will implement this later
-  };
+    const onSaveImageAsync = async () => {
+        try {
+            const localUri = await captureRef(imageRef, {
+                height: 440,
+                quality: 1,
+            });
+
+            await MediaLibrary.saveToLibraryAsync(localUri);
+            if (localUri) {
+                alert('Saved!');
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    };
 
 
     return (
         <View style={styles.container}>
             <View style={styles.imageContainer}>
-                <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
-               {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
+                <View ref={imageRef} collapsable={false}>
+                    <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
+                    {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
+                </View>
             </View>
             {showAppOptions ? (
                 <View style={styles.optionsContainer}>
