3086426784d2cdaf3fdc7d7361ee6977e0a4a7d7
Michael Luna <michaelluna244@gmail.com>
2025-05-10 18:48:06 -0700

    Refactor RootLayout to include GestureHandlerRootView; enhance EmojiSticker with double-tap scaling functionality.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,14 @@
 import { Stack } from "expo-router";
 import React from "react";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
-  );
+    return (
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <Stack>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                <Stack.Screen name="+not-found" />
+            </Stack>
+        </GestureHandlerRootView>
+    );
 }
