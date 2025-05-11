9fa76cbf31944dd215bea4858b62c240aee535e2
Michael Luna <michaelluna244@gmail.com>
2025-05-10 17:55:38 -0700

    Implement image picking functionality in Index screen; update Button and ImageViewer components to support selected images.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,35 +1,56 @@
 import { View, StyleSheet } from 'react-native';
 import ImageViewer from '@/components/ImageViewer';
-
 import Button from '@/components/Button';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
+
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 
 export default function Index() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
-      </View>
-      <View style={styles.footerContainer}>
-        <Button label="Choose a photo" theme= "primary" />
-        <Button label="Use this photo" />
-      </View>
-    </View>
-  );
+    const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+
+    const pickImageAsync = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ['images'],
+            allowsEditing: true,
+            quality: 1,
+        });
+
+        if (!result.canceled) {
+            setSelectedImage(result.assets[0].uri);
+        } else {
+            alert('You did not select any image.');
+        }
+    };
+    return (
+        <View style={styles.container}>
+            <View style={styles.imageContainer}>
+                <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
+            </View>
+            <View style={styles.footerContainer}>
+                <Button
+                    onPress={pickImageAsync}
+                    label="Choose a photo" theme="primary" />
+                <Button
+                    onPress={() => alert('You pressed a button.')}
+                    label="Use this photo" />
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-  },
-  imageContainer: {
-    flex: 1,
-  },
-  footerContainer: {
-    flex: 1 / 3,
-    alignItems: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#25292e',
+        alignItems: 'center',
+    },
+    imageContainer: {
+        flex: 1,
+    },
+    footerContainer: {
+        flex: 1 / 3,
+        alignItems: 'center',
+    },
 });
